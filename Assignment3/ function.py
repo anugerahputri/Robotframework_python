22bc3c27428e31a9e5dbfb980de6b0c9cfdc7309
anugerahputri <115148002+anugerahputri@users.noreply.github.com>
2025-02-06 07:52:04 +0700

    Update  function.py

--- Assignment3/ function.py
+++ Assignment3/ function.py
@@ -1,16 +1,13 @@
 def sort_numbers(arr):
-    # Separate even and odd numbers
+    #Filter angka 
     even_numbers = [num for num in arr if num % 2 == 0]
     odd_numbers = [num for num in arr if num % 2 != 0]
-
-    # Sort both lists in descending order
+     #urutkan angka 
     even_numbers.sort(reverse=True)
     odd_numbers.sort(reverse=True)
-
-    # Combine even and odd numbers (even first, then odd)
+    #gabungkan
     return even_numbers + odd_numbers
-
-# Example usage
+    
 input_array = [3, 2, 5, 1, 8, 9, 6]
 output = sort_numbers(input_array)
 print(output)
